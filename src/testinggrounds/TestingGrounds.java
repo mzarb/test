fe005fb23f45a48fa1621df32aef5033fa99dbb3
mz7371 <mz7371@RG40070.rgu.ac.uk>
2015-01-22 14:33:59 +0000

    Setup

---
+++ src/testinggrounds/TestingGrounds.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testinggrounds;
+
+/**
+ *
+ * @author mz7371
+ */
+public class TestingGrounds {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
